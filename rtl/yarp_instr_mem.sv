c7bf05923bb6ac55ab5e112395452020901063bc
Faraz Imran <farazimran18@icloud.com>
2025-04-16 21:18:18 -0400

    add yarp_instr_mem rtl and push .dvt/ files

--- rtl/yarp_instr_mem.sv
+++ rtl/yarp_instr_mem.sv
@@ -17,6 +17,15 @@ module yarp_instr_mem (
     output logic [31:0] instr_mem_instr_o
   );
 
-  // Write your logic here...
+  always_ff @(posedge clk or negedge reset_n) begin
+    if (!reset_n) begin
+      instr_mem_req_o <= '0;
+    end else begin
+      instr_mem_req_o <= '1;
+    end
+  end
+
+  assign instr_mem_addr_o  = instr_mem_pc_i;
+  assign instr_mem_instr_o = mem_rd_data_i;
 
 endmodule
