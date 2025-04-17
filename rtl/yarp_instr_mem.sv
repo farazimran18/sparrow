a592da382be39a4a56ab368d3f9c0d92bbc63978
Faraz Imran <farazimran18@icloud.com>
2025-04-16 22:48:40 -0400

    switch to iverilog, clean up code

--- rtl/yarp_instr_mem.sv
+++ rtl/yarp_instr_mem.sv
@@ -4,17 +4,13 @@ module yarp_instr_mem (
     input logic clk,
     input logic reset_n,
 
-    input logic [31:0] instr_mem_pc_i,
+    input logic [XLEN-1:0] instr_mem_pc_i,
 
-    // Output read request to memory
-    output logic        instr_mem_req_o,
-    output logic [31:0] instr_mem_addr_o,
+    output logic            instr_mem_req_o,
+    output logic [XLEN-1:0] instr_mem_addr_o,
+    input  logic [XLEN-1:0] mem_rd_data_i,
 
-    // Read data from memory
-    input logic [31:0] mem_rd_data_i,
-
-    // Instruction output
-    output logic [31:0] instr_mem_instr_o
+    output logic [XLEN-1:0] instr_mem_instr_o
   );
 
   always_ff @(posedge clk or negedge reset_n) begin
--- rtl/yarp_pkg.sv
+++ rtl/yarp_pkg.sv
@@ -1,3 +1,5 @@
 package yarp_pkg;
 
+  localparam int XLEN = 32;
+
 endpackage
