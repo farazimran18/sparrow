b81bbb5c80d9752c7fbf8d3a897374ff8dddec00
Faraz Imran <farazimran18@icloud.com>
2025-04-18 00:50:31 -0400

    format

--- rtl/yarp_decode.sv
+++ rtl/yarp_decode.sv
@@ -1,20 +1,22 @@
 import yarp_pkg::*;
 
 module yarp_decode (
-    input  logic [XLEN-1:0] instr_i,
-    output logic [4:0]      rs1_o,
-    output logic [4:0]      rs2_o,
-    output logic [4:0]      rd_o,
-    output logic [6:0]      op_o,
-    output logic [2:0]      funct3_o,
-    output logic [6:0]      funct7_o,
-    output logic            r_type_instr_o,
-    output logic            i_type_instr_o,
-    output logic            s_type_instr_o,
-    output logic            b_type_instr_o,
-    output logic            u_type_instr_o,
-    output logic            j_type_instr_o,
-    output logic [31:0]     instr_imm_o
+    input logic [XLEN-1:0] instr_i,
+
+    output logic [4:0]  rs1_o,
+    output logic [4:0]  rs2_o,
+    output logic [4:0]  rd_o,
+    output logic [6:0]  op_o,
+    output logic [2:0]  funct3_o,
+    output logic [6:0]  funct7_o,
+    output logic [31:0] instr_imm_o,
+
+    output logic r_type_instr_o,
+    output logic i_type_instr_o,
+    output logic s_type_instr_o,
+    output logic b_type_instr_o,
+    output logic u_type_instr_o,
+    output logic j_type_instr_o
   );
 
   riscv_op_e opcode;
