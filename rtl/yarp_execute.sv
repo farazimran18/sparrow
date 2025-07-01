ae368e48a9436a4ab6c54a6eb62c80ba73a2b324
Faraz Imran <farazimran18@icloud.com>
2025-07-01 17:45:03 -0400

    complete alu

--- rtl/yarp_execute.sv
+++ rtl/yarp_execute.sv
@@ -12,18 +12,23 @@ module yarp_execute
   alu_op_e alu_op;
   assign alu_op = alu_op_e'(op_sel_i);
 
+  logic [31:0] twos_compl_a, twos_compl_b;
+
+  assign twos_compl_a = opr_a_i[31] ? (~opr_a_i + 32'h1) : opr_a_i;
+  assign twos_compl_b = opr_b_i[31] ? (~opr_b_i + 32'h1) : opr_b_i;
+
   always_comb begin
     unique case (alu_op)
       OP_ADD:  alu_res_o = opr_a_i + opr_b_i;
       OP_SUB:  alu_res_o = opr_a_i - opr_b_i;
       OP_SLL:  alu_res_o = opr_a_i << opr_b_i[4:0];
-      OP_SRL:  alu_res_o = '1;
-      OP_SRA:  alu_res_o = '1;
-      OP_OR:   alu_res_o = '1;
-      OP_AND:  alu_res_o = '1;
-      OP_XOR:  alu_res_o = '1;
-      OP_SLTU: alu_res_o = '1;
-      OP_SLT:  alu_res_o = '1;
+      OP_SRL:  alu_res_o = opr_a_i >> opr_b_i[4:0];
+      OP_SRA:  alu_res_o = $signed(opr_a_i) >>> opr_b_i[4:0];
+      OP_OR:   alu_res_o = opr_a_i | opr_b_i;
+      OP_AND:  alu_res_o = opr_a_i & opr_b_i;
+      OP_XOR:  alu_res_o = opr_a_i ^ opr_b_i;
+      OP_SLTU: alu_res_o = {31'h0, opr_a_i < opr_b_i};
+      OP_SLT:  alu_res_o = {31'h0, twos_compl_a < twos_compl_b};
       default: alu_res_o = '0;
     endcase
   end
