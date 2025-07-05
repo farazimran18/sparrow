0e13dd2bfe58226fd39ff5d9fa5bc80e8fba8f42
Faraz Imran <farazimran18@icloud.com>
2025-07-05 04:48:31 -0400

    time 2 sleep...

--- rtl/sparrow_decode.sv
+++ rtl/sparrow_decode.sv
@@ -11,12 +11,12 @@ module sparrow_decode
     output logic      [ 6:0] o_funct7,
     output logic      [31:0] o_instr_imm,
 
-    output logic o_r_type_instr,
-    output logic o_i_type_instr,
-    output logic o_s_type_instr,
-    output logic o_b_type_instr,
-    output logic o_u_type_instr,
-    output logic o_j_type_instr
+    output logic o_instr_r_type,
+    output logic o_instr_i_type,
+    output logic o_instr_s_type,
+    output logic o_instr_b_type,
+    output logic o_instr_u_type,
+    output logic o_instr_j_type
   );
 
   assign o_rs1    = i_instr[19:15];
@@ -27,36 +27,36 @@ module sparrow_decode
   assign o_funct7 = i_instr[31:25];
 
   always_comb begin
-    o_r_type_instr = '0;
-    o_i_type_instr = '0;
-    o_s_type_instr = '0;
-    o_b_type_instr = '0;
-    o_u_type_instr = '0;
-    o_j_type_instr = '0;
+    o_instr_r_type = '0;
+    o_instr_i_type = '0;
+    o_instr_s_type = '0;
+    o_instr_b_type = '0;
+    o_instr_u_type = '0;
+    o_instr_j_type = '0;
     o_instr_imm    = '0;
 
     unique case (o_op)
       R_TYPE: begin
-        o_r_type_instr = '1;
+        o_instr_r_type = '1;
       end
       I_TYPE_0, I_TYPE_1, I_TYPE_2: begin
-        o_i_type_instr = '1;
+        o_instr_i_type = '1;
         o_instr_imm    = {{20{i_instr[31]}}, i_instr[31:20]};
       end
       S_TYPE: begin
-        o_s_type_instr = '1;
+        o_instr_s_type = '1;
         o_instr_imm    = {{20{i_instr[31]}}, i_instr[31:25], i_instr[11:7]};
       end
       B_TYPE: begin
-        o_b_type_instr = '1;
+        o_instr_b_type = '1;
         o_instr_imm    = {{20{i_instr[31]}}, i_instr[7], i_instr[30:25], i_instr[11:8], 1'b0};
       end
       U_TYPE_0, U_TYPE_1: begin
-        o_u_type_instr = '1;
+        o_instr_u_type = '1;
         o_instr_imm    = {i_instr[31:12], 12'h0};
       end
       J_TYPE: begin
-        o_j_type_instr = '1;
+        o_instr_j_type = '1;
         o_instr_imm    = {{12{i_instr[31]}}, i_instr[19:12], i_instr[20], i_instr[30:21], 1'b0};
       end
       default: ;
